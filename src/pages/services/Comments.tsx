514f263ba3f645e8488bd65fdccad18eff9745c0
Uday Hasan <udayhasan15@gmail.com>
2024-05-07 01:13:37 +0600

    optional chaining in single service pageg

--- src/pages/services/Comments.tsx
+++ src/pages/services/Comments.tsx
@@ -2,30 +2,37 @@ import { useComment } from "@/contexts/comments-context/CommentContext";
 
 const Comments = ({ productId }: { productId?: string }) => {
   const { comments } = useComment();
-  const productComments = comments.filter(
+  const productComments = comments?.filter(
     (comment) => comment.productId === productId
   );
   return (
-    <div className="px-4">
+    <div className="px-4 my-4">
       <h1 className="text-3xl">Review(s)</h1>
       <div>
-        {productComments?.map((comment) => {
-          const time = new Date(comment.createdAt);
-          console.log(time);
-          return (
-            <div key={comment._id} className="">
-              <div className="text-base font-bold flex justify-between">
-                <h1>By: {comment.user.name}</h1>
-                <h1>Posted: {time.toLocaleDateString()}</h1>
-              </div>
-              <div>
-                <h1 className="font-semibold">{comment.comment}</h1>
-              </div>
-            </div>
-          );
-        })}
+        {!productComments?.length ? (
+          <h1 className="text-3xl font-semibold">No comments... ... ...</h1>
+        ) : (
+          <div>
+            {productComments?.map((comment) => {
+              const time = new Date(comment.createdAt);
+              return (
+                <div key={comment._id} className="">
+                  <div className="text-base font-bold flex justify-between">
+                    <h1>By: {comment.user.name}</h1>
+                    <h1>Posted: {time.toLocaleDateString()}</h1>
+                  </div>
+                  <div>
+                    <h1 className="font-semibold">{comment?.comment}</h1>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
-      <hr className="border my-2 border-cBlue border-dashed" />
+      {productComments?.length && (
+        <hr className="border my-2 border-cBlue border-dashed" />
+      )}
     </div>
   );
 };
